794bc8f7126838c20effcb01ef14858e1ce84f06
Frank Ehmann <frank.ehmann@gmail.com>
2026-03-15 15:30:38 +0100

    enemy path and target following

--- char/char_base.gd
+++ char/char_base.gd
@@ -5,6 +5,14 @@ class_name CharBase
 @export var stats: CharStats
 @export var state_machine: CharStateMachine = CharStateMachine.new()
 @export var char_name: String
+@export var path: Path2D
+var next_pos: Vector2
+var path_points: PackedVector2Array
+var path_idx: int = 0
+var path_dir: int = 1
+@export var update_dist: float = 10.0
+@export var detection_area: Area2D
+var target: Node = null
 
 @export var animation: AnimatedSprite2D:
 	set(a):
@@ -26,10 +34,74 @@ var death_timer: Timer = Timer.new()
 func _ready() -> void:
 	state_machine.check_functions[CharStateMachine.StateType.DIE] = func(): return stats.hp <= 0
 	state_machine.entry_functions[CharStateMachine.StateType.DIE] = func(): death_timer.start(1.0)
+	state_machine.process_functions[CharStateMachine.StateType.MOVE] = move_npc
+	state_machine.check_functions[CharStateMachine.StateType.MOVE] = check_move
+	state_machine.check_functions[CharStateMachine.StateType.ATTACK] = check_attack
 	death_timer.one_shot = true
 	death_timer.timeout.connect(func(): queue_free())
 	add_child(death_timer)
 	update_animation()
+	check_path()
+	if detection_area:
+		detection_area.body_entered.connect(_on_target_body_entered)
+		detection_area.body_exited.connect(_on_target_body_exited)
+
+func check_path() -> void:
+	if path:
+		path_points = (path.curve.get_baked_points() as Array).map(func (e): return e + path.global_position)
+	print("path available: %s with %d points" %  [path, path_points.size()])
+
+func update_path() -> bool:
+	next_pos = path_points[path_idx]
+	var diff = next_pos - global_position
+	if diff.length() < update_dist:
+		if path_idx + path_dir >= path_points.size() or path_idx + path_dir < 0:
+			path_dir = - path_dir
+		path_idx += path_dir
+		return true
+	direction = diff.normalized()
+	return false
+
+''' state machine override functions '''
+func move_npc(_target: Node) -> void:
+	if target:
+		direction = (target.global_position - global_position).normalized()
+		if direction:
+			animation.flip_h = direction.x < 0
+			hurt.scale.x = sign(direction.x) * abs(hurt.scale.x)
+			velocity.x = direction.x * capabilities.speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, capabilities.speed)
+		print("%s t direction %s velocity %s" % [name, direction, velocity])
+		return
+	if path:
+		update_path()
+		if direction:
+			velocity.x = direction.x * capabilities.speed
+			animation.flip_h = direction.x < 0
+		else:
+			velocity.x = move_toward(velocity.x, 0, capabilities.speed)
+		print("p direction %s velocity %s" % [direction, velocity])
+	else:
+		direction = (next_pos - global_position).normalized()
+		print("e direction %s velocity %s" % [direction, velocity])
+	'''
+func handle_move(_target: Hero) -> void:
+	direction.x = Input.get_axis("move_left","move_right")
+	if direction:
+		animation.flip_h = direction.x < 0
+		hurt.scale.x = sign(direction.x) * abs(hurt.scale.x)
+		velocity.x = direction.x * capabilities.speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, capabilities.speed)
+		'''
+func check_move() -> bool:
+	if path:
+		return path_points.size() > 0
+	else:
+		return target != null
+func check_attack() -> bool:
+	return false
 
 func update_animation() -> void:
 	if animation.sprite_frames.has_animation(state_machine.animations[state_machine.current]):
@@ -51,14 +123,30 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	move_and_slide()
 
+func _on_target_body_entered(_target: Node2D) -> void:
+	print("_on_target_body_entered(%s) of %s" % [_target, name])
+	for g in get_groups():
+		if _target.is_in_group(g):
+			print("nah, familiar")
+			return
+	print("target itentified")
+	target = _target
+	next_pos == target.global_position
+
+func _on_target_body_exited(_target: Node2D) -> void:
+	print("_on_target_body_exited(%s) of %s" % [_target, name])
+	if _target == target:
+		print("clearing target")
+		target = null
+
 func _on_area_entered(area: Area2D) -> void:
 	print("_on_area_entered(%s) of %s" % [area, name])
 	pass
-	
+
 func _on_body_entered(body: Node) -> void:
 	print("_on_body_entered(%s) of %s" % [body, name])
 	pass
-	
+
 func _on_area_exited(_area: Area2D) -> void:
 	pass
 
